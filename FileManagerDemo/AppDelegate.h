7defcce112b9be0ed4992cbe5c7534e448ba82b1
huangbo <huangbo@bogon.(none)>
2013-01-22 10:39:07 +0800

    Initial Commit

---
+++ FileManagerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileManagerDemo
+//
+//  Created by huangbo on 13-1-22.
+//  Copyright (c) 2013年 huangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FileManagerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FileManagerDemo
+//
+//  Created by huangbo on 13-1-22.
+//  Copyright (c) 2013年 huangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
